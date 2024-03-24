640afa53bc18a4b605ea8cc4447f5e0339efa306
Maheedhar15 <maheedhareducation@gmail.com>
2024-03-24 20:45:45 +0530

    made changes for task-9

---
+++ git_file_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone https://github.com/Maheedhar15/Concourse-Practice.git edited-repo
+cd edited-repo
+# Edit the file (for example, using sed)
+echo "Before Change"
+cat edit_this_file.txt
+sed -i 's/Almighty/Chibaku/g' edit_this_file.txt
+echo "After Change"
+cat edit_this_file.txt
+# Commit the changes
+git config --global user.email "maheedhareducation@gmail.com"
+git config --global user.name "Maheedhar15"
+git commit -am "Update edit_this_file.txt"
+          # Push the changes back to the repository
+git push origin main  # or specify the branch you are working with
